e8508c4a92c042dc17ed9004d8e9a5ceb6ffec3c
Quennieannbaculio <79793609+Quennieannbaculio@users.noreply.github.com>
2021-12-01 05:41:42 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 int main() {
-  int n;
-  double arr[100];
-  printf("Enter the number of elements (1 to 100): ");
-  scanf("%d", &n);
+  int r, c, a[100][100], b[100][100], sum[100][100], i, j;
+  printf("Enter the number of rows (between 1 and 100): ");
+  scanf("%d", &r);
+  printf("Enter the number of columns (between 1 and 100): ");
+  scanf("%d", &c);
 
-  for (int i = 0; i < n; ++i) {
-    printf("Enter number %d: ", i + 1);
-    scanf("%lf", &arr[i]);
-  }
-
-  // storing the largest number to arr[0]
-  for (int i = 1; i < n; ++i) {
-    if (arr[0] < arr[i]) {
-      arr[0] = arr[i];
+  printf("\nEnter elements of 1st matrix:\n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("Enter element a%d%d: ", i + 1, j + 1);
+      scanf("%d", &a[i][j]);
     }
-  }
 
-  printf("\nLargest element = %.2lf\n\n\n", arr[0]);
+  printf("Enter elements of 2nd matrix:\n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("Enter element b%d%d: ", i + 1, j + 1);
+      scanf("%d", &b[i][j]);
+    }
 
-  return 0;
-};
+  // adding two matrices
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      sum[i][j] = a[i][j] + b[i][j];
+    }
+
+  // printing the result
+  printf("\nSum of two matrices: \n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("%d   ", sum[i][j]);
+      if (j == c - 1) {
+        printf("\n\n");
+      }
+
+
+    return 0 ;
+    };
